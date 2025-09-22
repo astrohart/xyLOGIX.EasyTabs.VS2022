67e47d47f03a7f751a353f2a1ccbd340e6cdd10b
Brian Hart <brian.hart@xylogix.net>
2025-09-21 18:19:00 -0600

    Update TitleBarTabEventArgs.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- xyLOGIX.EasyTabs/TitleBarTabEventArgs.cs
+++ xyLOGIX.EasyTabs/TitleBarTabEventArgs.cs
@@ -10,21 +10,21 @@ namespace xyLOGIX.EasyTabs
     public class TitleBarTabEventArgs : EventArgs
     {
         /// <summary>Action that is being performed.</summary>
-        public TabControlAction? Action { get; set; }
+        public TabControlAction? Action { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>
         /// The tab that the <see cref="P:xyLOGIX.EasyTabs.TitleBarTabEventArgs.Action" />
         /// is being performed on.
         /// </summary>
-        public TitleBarTab Tab { get; set; }
+        public TitleBarTab Tab { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>Index of the tab within the collection.</summary>
-        public int TabIndex { get; set; }
+        public int TabIndex { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>
         /// Flag indicating if the user was dragging the tab when the event
         /// occurred.
         /// </summary>
-        public bool WasDragging { get; set; }
+        public bool WasDragging { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
     }
 }
